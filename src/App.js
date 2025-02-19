16b29eb650d59747821b9db93164e95c6f6973a7
alexander-laiman <79998793+alexander-laiman@users.noreply.github.com>
2025-02-19 03:13:38 -0500

    Add gitignore and commit react site files.

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import portrait from "./img/1718258605229.jpg";
+import linkedinlogo from "./img/LinkedIn-Logos/In/Digital/White/2x/In-White-128@2x.png";
+import githublogo from "./img/GitHub Logos/SVG/GitHub_Invertocat_Light.svg";
+import paperemoji from "./img/page_facing_up.svg";
+import imgtest from "./img/_DSF4084.JPG";
+import imgtest2 from "./img/_DSF4079.JPG";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={portrait} className="App-logo" alt="logo" />
+        <h>Alexander Laiman</h>
+        <div className="socialLinks">
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/alexander-laiman-a698b0103/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedinlogo} className="LinkLogos" alt="Linkedin" />
+          </a>
+          <a
+            className="App-link"
+            href="https://github.com/alexander-laiman"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githublogo} className="LinkLogos" alt="Github" />
+          </a>
+          <a
+            className="App-link"
+            href="https://drive.google.com/file/d/1ii2J-1499_3pgfik1tTDMnEks5dGLZHq/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={paperemoji} className="LinkLogos" alt="resume" />
+          </a>
+        </div>
+        <div className="projectTiles">
+          <img src={imgtest} className="Project-Preview" />
+          <img src={imgtest2} className="Project-Preview" />
+          <img src={imgtest} className="Project-Preview" />
+          <img src={imgtest2} className="Project-Preview" />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
