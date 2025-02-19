ff10e31ff76b0617a4ec9f23f362e73cd1d646b7
alexander-laiman <79998793+alexander-laiman@users.noreply.github.com>
2025-02-19 05:54:34 -0500

    Many photos and gallery factoring of js layout

--- src/App.js
+++ src/App.js
@@ -1,52 +1,72 @@
-import portrait from "./img/1718258605229.jpg";
 import linkedinlogo from "./img/LinkedIn-Logos/In/Digital/Blue/2x/In-Blue-128-Γö¼┬½@2x.png";
 import githublogo from "./img/GitHub Logos/SVG/GitHub_Invertocat_Dark.svg";
 import paperemoji from "./img/page_facing_up.svg";
-import imgtest from "./img/DSF4084.JPG";
-import imgtest2 from "./img/DSF4079.JPG";
+
+import papertex from "./img/paper-texture_1194-5999.jpg";
 import "./App.css";
+
+const images = require.context("./img/gallery", false);
+const imageList = images.keys().map((image) => images(image));
+
+function ImageGallery() {
+  return (
+    <div>
+      {imageList.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`image-${index}`}
+          className="Project-Preview fade-in"
+        />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="spanSpace" />
-        {/* <img src={portrait} className="App-logo" alt="logo" /> */}
-        <h>Alexander Laiman</h>
-        <div className="socialLinks">
-          <a
-            className="App-link"
-            href="https://www.linkedin.com/in/alexander-laiman-a698b0103/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={linkedinlogo} className="LinkLogos" alt="Linkedin" />
-          </a>
-          <a
-            className="App-link"
-            href="https://drive.google.com/file/d/1ii2J-1499_3pgfik1tTDMnEks5dGLZHq/view?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={paperemoji} className="LinkLogos1" alt="resume" />
-          </a>
-          <a
-            className="App-link"
-            href="https://github.com/alexander-laiman"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={githublogo} className="LinkLogos" alt="Github" />
-          </a>
-        </div>
-        <div className="projectTiles">
-          <header className="projectHeaders">Street</header>
-          <img src={imgtest} className="Project-Preview fade-in" />
-          <img src={imgtest2} className="Project-Preview fade-in" />
-          <img src={imgtest} className="Project-Preview fade-in" />
-          <img src={imgtest2} className="Project-Preview fade-in" />
-        </div>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <div className="spanSpace" />
+          {/* <img src={portrait} className="App-logo" alt="logo" /> */}
+          <header>Alexander Laiman</header>
+          <div className="socialLinks">
+            <a
+              className="App-link"
+              href="https://www.linkedin.com/in/alexander-laiman-a698b0103/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedinlogo} className="LinkLogos" alt="Linkedin" />
+            </a>
+            <a
+              className="App-link"
+              href="https://drive.google.com/file/d/1ii2J-1499_3pgfik1tTDMnEks5dGLZHq/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={paperemoji} className="LinkLogos1" alt="resume" />
+            </a>
+            <a
+              className="App-link"
+              href="https://github.com/alexander-laiman"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={githublogo} className="LinkLogos" alt="Github" />
+            </a>
+          </div>
+          <div style={{ height: "40px" }}></div>
+
+          <div className="projectTiles">
+            <header className="projectHeaders"></header>
+            <ImageGallery />
+          </div>
+          <header>Thank you.</header>
+          <div className="spanSpace" />
+        </header>
+      </div>
     </div>
   );
 }
